0b1d0c724cf0d234844a72da90dc54dd96f1c5c6
Tomas Gonzalez <tomasgonzalez429030@gmail.com>
2023-11-07 13:07:10 -0300

    se agregan las funciones register y home

--- vista/vista.php
+++ vista/vista.php
@@ -13,9 +13,15 @@ class vista{
         $this->smart->display('template/header.tpl');
 
     }
+    function showHome(){
+        $this->smart->display('template/home.tpl');
+    }
     function showLogin(){
         $this->smart->display('template/login.tpl');
 
+    }
+    function showRegister(){
+        $this->smart->display('template/register.tpl');
     }
     function showFooter(){
         $this->smart->display('template/footer.tpl');

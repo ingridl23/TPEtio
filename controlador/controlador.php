93a53a9669a709836541fe004400968ef280b1f4
Tomas Gonzalez <tomasgonzalez429030@gmail.com>
2023-11-08 14:46:30 -0300

    se modifico controlador y gitignore

--- controlador/controlador.php
+++ controlador/controlador.php
@@ -1,58 +1,80 @@
 <?php
 
 include_once "modelo/modelo.php";
+require_once "vista/vista.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-    $email = $_POST['email'];
-    $contraseña = $_POST['contraseña'];
+class UserController {
+    private $vista;
 
-    $usuarioNuevo = new modelo();
-
-    try {
-        $usuarioNuevo->BaseDeDatos($nombre, $apellido, $email, $contraseña);
-
-        session_start();
-        $_SESSION['nombre'] = $nombre;
-        $_SESSION['apellido'] = $apellido;
-        $_SESSION['email'] = $email;
-        header("Location: vista.php");
-        die(); 
-    } catch (Exception $e) {
-        echo "¡Error al registrar usuario: " . $e->getMessage();
+    public function __construct() {
+        $this->vista = new Vista();
     }
-} else {
-    echo "¡Problemas al registrarse!"; 
-    //por ejemplo el correo ya fue registrado en la base de datos
-}
-// Inicio de sesión
-if (isset($_POST['login'])) {
-    $usuario = $_POST['usuario'];
-    $contraseña = $_POST['contraseña'];
 
-    $usuarioEncontrado = $miModelo->getUserByPassword($usuario);
+    public function mostrarHome() {
 
-    if ($usuarioEncontrado && password_verify($contraseña, $usuarioEncontrado->contraseña)) {
+        $this->vista->showHeader();
+        $this->vista->showHome();  
+        $this->vista->showFooter();
+    }
 
-        session_start();
-        $_SESSION['usuario'] = $usuarioEncontrado;
+    public function mostrarLogin() {
 
-        header('Location: home.php'); //adaptar a smarty
-        exit();
-    } else {
-    
-        echo "Nombre de usuario o contraseña incorrectos";
+        $this->vista->showHeader();
+        $this->vista->showLogin();
+        $this->vista->showFooter();
+    }
+
+    public function mostrarRegistro() {
+
+        $this->vista->showHeader();
+        $this->vista->showRegistrer(); 
+        $this->vista->showFooter();
+    }
+
+    public function procesarRegistro() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $contraseña = $_POST['contraseña'];
+
+            $usuarioNuevo = new modelo();
+
+            try {
+                $usuarioNuevo->BaseDeDatos($nombre, $apellido, $email, $contraseña);
+
+                session_start();
+                $_SESSION['nombre'] = $nombre;
+                $_SESSION['apellido'] = $apellido;
+                $_SESSION['email'] = $email;
+                header("Location: vista.php");
+                die(); 
+            } catch (Exception $e) {
+                echo "¡Error al registrar usuario: " . $e->getMessage();
+            }
+        } else {
+            echo "¡Problemas al registrarse!"; 
+        }
+    }
+
+    public function iniciarSesion() {
+        if (isset($_POST['login'])) {
+            $usuario = $_POST['usuario'];
+            $contraseña = $_POST['contraseña'];
+
+            $miModelo = new modelo(); 
+
+            $usuarioEncontrado = $miModelo->getUserByPassword($usuario);
+
+            if ($usuarioEncontrado && password_verify($contraseña, $usuarioEncontrado->contraseña)) {
+                session_start();
+                $_SESSION['usuario'] = $usuarioEncontrado;
+                header('Location: home.php'); 
+                exit();
+            } else {
+                echo "Nombre de usuario o contraseña incorrectos";
+            }
+        }
     }
 }
-
 ?>
-
-
-
-
-
-
-
-
-
